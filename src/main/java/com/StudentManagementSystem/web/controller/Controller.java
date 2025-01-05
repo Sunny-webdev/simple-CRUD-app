6c6a645e8d894c93d4bc3c4a6013bf20ad854f67
Sunny-webdev <arvindrockzz777@gmail.com>
2025-01-05 21:33:27 +0530

    Sixteenth commit

--- src/main/java/com/StudentManagementSystem/web/controller/Controller.java
+++ src/main/java/com/StudentManagementSystem/web/controller/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.StudentManagementSystem.web.entity.Student;
 import com.StudentManagementSystem.web.service.StudentService;
@@ -16,10 +17,10 @@ public class Controller {
 	@Autowired
 	private StudentService service;
 	
-	@GetMapping("/home")
-	public String home() {
+	@GetMapping("/")
+	public RedirectView home() {
 		
-		return "home";
+		return new RedirectView("/students");
 	}
 	
 	@GetMapping("/students")
